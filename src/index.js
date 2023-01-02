9b5b4143c8a6e963771343ae891475117bc18fa3
Juszuf Arsunkaev <81538739+Jarsunkaev@users.noreply.github.com>
2023-01-02 16:12:50 +0100

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import { Application, Container, Loader, Texture, TilingSprite } from 'pixi.js'
+import { Application, Container } from 'pixi.js';
 import Enemy from './Enemy';
 import Player from './Player';
 import Background from './Background';
 import Rocket from './Rocket';
 
-// Create new PIXI application
 const app = new Application({
   width: 800,
   height: 600,
@@ -19,71 +18,36 @@ app.renderer.view.style.left = "50%";
 app.renderer.view.style.top = "50%";
 app.renderer.view.style.transform = "translate3d( -50%, -50%, 0 )";
 
-// Init game container (where objects are displayed)
 const stage = new Container();
 
-
-// Enemy entity
 let enemy;
-
-//rocket entity
 let rocket;
-
-// Player entity
 let spaceship;
-
-//Background entity
 let background;
 
-
-// Load assets (empty here), and then call init function below
-const loader = Loader.shared;
-loader.add([
+Loader.shared.add([
   "assets/Ship6.png",
   "assets/Ship1.png",
   "assets/laser.png",
 ]).load(init);
 
-// First function called after loading assets is done
 function init() {
-  
-    //Init background
-    background = new Background(stage);
+  background = new Background(stage);
+  enemy = new Enemy(stage);
+  rocket = new Rocket(stage);
+  spaceship = new Player(stage);
 
-    // Init enemy 
-    enemy = new Enemy(stage);
-
-    //init rocket 
-
-    rocket = new Rocket(stage)
-
-    // Init player spaceship
-    spaceship = new Player(stage);
-
-  
-    app.renderer.render(stage);
-
-
-    requestAnimationFrame(loop);
+  app.renderer.render(stage);
+  requestAnimationFrame(loop);
 }
 
-
-// Looping function, called every frame
 function loop() {
-
   enemy.update();
   spaceship.update();
   background.update();
 
-
-
   requestAnimationFrame(loop);
-
   app.renderer.render(stage);
 }
 
-
-
-
-
-document.body.appendChild(app.view)
+document.body.appendChild(app.view);
